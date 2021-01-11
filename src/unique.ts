d398596c121e34f7ce6208f3910b00654be9826b
Miguel Sanz <msanz1610@gmail.com>
2021-01-10 22:23:04 -0300

    feat: add helper unique filter

---
+++ src/unique.ts
@@ -0,0 +1,10 @@
+import _ from "lodash";
+
+const uniquePipe = (value: any, arg: string) => {
+  if (value !== undefined && value !== null) {
+    return _.uniqBy(value, arg);
+  }
+  return value;
+};
+
+export default uniquePipe;
